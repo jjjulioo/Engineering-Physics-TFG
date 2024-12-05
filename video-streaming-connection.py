7881e7a915e338e85a662c0859064a47af082567
jjjulioo <105503018+jjjulioo@users.noreply.github.com>
2024-12-04 19:54:11 -0600

    video streaming done

--- video-streaming-connection.py
+++ video-streaming-connection.py
@@ -1,10 +1,19 @@
 import numpy as np
 import matplotlib as plt
 import cv2 as cv
-print("OpenCV version:", cv.__version__)
 
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture(1)
 
-while True:
+while True: 
     ret, frame = cap.read()
+
+    image = np.zeros(frame.shape, np.uint8)
     
+    cv.imshow('frame', frame)
+
+    if cv.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
+
