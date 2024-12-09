1d3c84c67bba1df863b08683a3f1c712c9a7b662
jjjulioo <105503018+jjjulioo@users.noreply.github.com>
2024-12-08 23:38:37 -0600

    lineas

--- video-streaming-connection.py
+++ video-streaming-connection.py
@@ -7,6 +7,20 @@ cap = cv.VideoCapture(1)
 while True: 
     ret, frame = cap.read()
 
+    # Get frame dimensions
+    height, width, _ = frame.shape
+
+     # Calculate the middle of the frame
+    middle_x = width // 2
+    middle_y = height // 2
+
+
+     # Draw a red vertical line in the middle of the frame
+    color = (0, 0, 255)  # Red in BGR
+    thickness = 2  # Thickness of the line
+    cv.line(frame, (middle_x, 0), (middle_x, height), color, thickness)
+    cv.line(frame, (0, middle_y), (width, middle_y), color, thickness)
+
     image = np.zeros(frame.shape, np.uint8)
     
     cv.imshow('frame', frame)
